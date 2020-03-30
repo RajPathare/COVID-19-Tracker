5818de867e7cc49a51a830ded0bbe5a21b645871
RajPathare <rajgpathare@gmail.com>
2020-03-31 00:54:32 +0530

    added suggestions block

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,9 @@ import React from 'react';
 const Title = () => {
     return(
         <div className="container">
-        <div className="jumbotron"  style={{height:"150px", color:"white", backgroundColor:"#595959"}}><h2 style={{display: 'flex', justifyContent: 'center'}}>COVID-19 Info and Tracking</h2>
+        <div className="jumbotron"  style={{height:"170px", color:"white", backgroundColor:"#595959"}}><h2 style={{display: 'flex', justifyContent: 'center'}}>COVID-19 Info and Tracking</h2>
         <h6 style={{ textAlign:"right"}}>Created by - Raj Pathare</h6>
+        <h6 style={{ textAlign:"right", fontSize:"13px"}}><a style={{ color:"white"}} href="https://docs.google.com/forms/d/e/1FAIpQLSdwLpcvf9QtYZl6IBpjTqB46Kay5XjN-f36nPXjiZHOX9s2GA/viewform?usp=sf_link" target="_blank">Click here for any suggestions/feedback</a></h6>
         </div>
         </div>
     )
