99052b5df9a2b3b7356fbc34bdf5b9901c9684a3
RajPathare <rajgpathare@gmail.com>
2020-04-17 10:44:52 +0530

    changed api route

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import corona from '../api/corona';
 export const fetchList = () => {
     
     return async (dispatch, getState) => {
-        const response = await corona.get('/countries?sort=cases');
+        const response = await corona.get('/v2/countries?sort=cases');
 
         dispatch({ type:'FETCH_LIST', payload: response.data })
     } 
@@ -12,7 +12,7 @@ export const fetchList = () => {
 
 export const fetchTotalCases = () => {
     return async (dispatch, getState) => {
-        const response = await corona.get('/all');
+        const response = await corona.get('/v2/all');
 
         dispatch({ type:'FETCH_TOTAL', payload: response.data })
     } 
